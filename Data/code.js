cbb9cbbf61f694ea677b0e9f98bee863dd2e3869
Mohamed Al Hamzy <102090177+Alhamzy@users.noreply.github.com>
2024-06-09 17:00:35 +0400

    First Commit

---
+++ Data/code.js
@@ -0,0 +1,9 @@
+function(){
+    var button = document.getElementsByClassName('Button-sc-qlcn5g-0')[0];
+
+    if (button) {
+        button.click();
+    } else {
+        console.log('Button not found');
+    }
+}
